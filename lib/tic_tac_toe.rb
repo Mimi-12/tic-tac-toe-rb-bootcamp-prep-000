92f6b9b698422f0e3f590f15a83d9c9ee5b3bead
selam lemma <oneiconforlife26@gmail.com>
2020-10-16 19:18:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,6 @@ WIN_COMBINATIONS = [
  def move(board,index,token)
     board[index] = token
   end
-  def position_taken?(index,board)
+  def position_taken?(board,index)
       !(board[index].nil? || board[index] == " ")
     end
