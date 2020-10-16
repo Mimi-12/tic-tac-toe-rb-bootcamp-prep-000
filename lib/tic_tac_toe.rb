67e89f2bfe2e0741aa98705b3b8ca31a178a09c9
selam lemma <oneiconforlife26@gmail.com>
2020-10-16 21:51:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,6 @@ def winner(board)
 end
 
 def play(board)
-   turn(board) until over?(board)
+   turn?(board) until over?(board)
    puts draw?(board)? "Cat's Game!" : "Congratulations #{winner}!"
  end
