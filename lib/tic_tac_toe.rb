e01018d924c0a7d11677f6ef8a2cecc2b43fe4da
selam lemma <oneiconforlife26@gmail.com>
2020-10-16 19:30:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,7 @@ WIN_COMBINATIONS = [
 def turn_count(board)
     board.count{|token| token == "X" || token == "O"}
  end
+
  def current_player(board)
-     turn_count.even? ? "X" : "O"
+     turn_count(board).even? ? "X" : "O"
    end
