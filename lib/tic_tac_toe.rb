1a377978fb7f15e2b7fdfeaa4143cd27c0797db9
selam lemma <oneiconforlife26@gmail.com>
2020-10-16 19:45:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,7 @@ def turn_count(board)
 def turn(board)
    puts "Please enter 1-9:"
    input = gets.strip
+   index = input_to_index(input)
    if !valid_move?(board, input)
     turn(board)
   end
